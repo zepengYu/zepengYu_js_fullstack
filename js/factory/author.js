4ebb2aa8f531052e5178573e64b24c023d1f13cb
zepengYu <2549793618@qq.com>
2018-04-21 16:35:03 +0800

    js工厂

---
+++ js/factory/author.js
@@ -0,0 +1,30 @@
+function Person(name){
+    this.name = name
+}
+Person.prototype.getName = function(){
+    return this.name
+}
+
+// IT工程师
+// extend Person
+function Coder(name,languages){
+// call?
+Person.call(this,name)
+// 父类没有可以加
+// 语言
+this.languages = languages
+}
+// new person 新的对象就是coder的原型对象
+Coder.prototype = new Person()
+Coder.prototype.construtor = Coder
+Coder.prototype.getLanguages = function(){
+    console.log(this.languages)
+}
+
+var Wxf = new Person('汪西发');
+console.log(Wxf.name);
+
+var xjy = new Coder('徐加元',['javascript','python','c/c++']);
+console.log(xjy.name + ' ' + xjy.languages.join(','))
+console.log(xjy.getName());
+console.log(xjy.getLanguages());
