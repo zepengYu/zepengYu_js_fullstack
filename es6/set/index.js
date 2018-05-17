7c8b19ae84a4e481bbc85f89459af8ff05eb100c
zepengYu <2549793618@qq.com>
2018-05-17 22:06:19 +0800

    set

---
+++ es6/set/index.js
@@ -0,0 +1,15 @@
+//set 
+let mySet = new Set();
+mySet.add(1);
+mySet.add(5);
+mySet.add(5);
+
+for(const num of mySet){
+    console.log(num);
+}
+//has返回值
+console.log(mySet.has(Math.sqrt(25)));
+console.log(mySet.size);
+mySet.delete(5);
+mySet.has(5);
+console.log(mySet.size);
