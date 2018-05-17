8cc1b13631c66df566a120f20267a50e83c6abd7
zepengYu <2549793618@qq.com>
2018-05-17 22:10:46 +0800

    starter

--- weapp/wxapp-starter/pages/index/index.js
+++ weapp/wxapp-starter/pages/index/index.js
@@ -1,77 +1,74 @@
-const app = getApp()
-
-import{
+import {
   API_BASE
 } from '../../config/api'
 
+const app = getApp()
+
 Page({
   data: {
-    entities:[],
-    isLoading:true,
-    currentPage:1,
-    totalPages:1,
-    total:0
+    entities: [],
+    isLoading: true,
+    currentPage: 1,
+    totalPages: 1,
+    total: 0
   },
-  onPullDownRefresh(){
-    // console.log('下拉');
+  onPullDownRefresh() {
+    // console.log('下拉了');
     wx.request({
       url: API_BASE,
-      success:(res) => {
+      success: (res) => {
+        // console.log(res);
         this.setData({
-          isLoading:false,
-          entities:res.data.data.articles,
-          currentPage:1,
-          totalPages:res.data.data.totalPages,
-          total:res.data.data.total
+          isLoading: false,
+          entities: res.data.data.articles,
+          currentPage: 1
         })
-      }
-    })
-      
+      }})
   },
   onLoad () {
-   wx.request({
-    url:API_BASE,
-    success:(res) => {
-      this.setData({
-        isLoading:false,
-        entities:res.data.data.articles,
-        currentPage: 1,
-        totalPages: res.data.data.totalPages,
-        total: res.data.data.total
-      })
-    }
-  })
+    wx.request({
+      url: API_BASE,
+      success: (res) => {
+        // console.log(res);
+        this.setData({
+          isLoading: false,
+          entities: res.data.data.articles,
+          currentPage: 1,
+          totalPages: res.data.data.totalPages,
+          total: res.data.data.total
+        })
+      }})
   },
   onReachBottom () {
     // console.log('到底了');
     // 请求下一页的数据
     // 最后一页
-    // model？
-    // this.data.currentPage
+    // model? 
     // 解构json
-    let {currentPage, totalPages, isLoading} = this.data
+    let { currentPage, totalPages, isLoading } = this.data
     console.log(currentPage, totalPages, isLoading);
-    if (currentPage >= totalPages || isLoading){
+    if ( currentPage >= totalPages || isLoading) {
       return;
     }
     currentPage += 1
     this.setData({
-      isLoading:true
+      isLoading: true
     })
     const url = `${API_BASE}/${currentPage}`;
     wx.request({
-      url:API_BASE,
-      success:(res)=>{
+      url: API_BASE,
+      success: (res) => {
         const entities = [
           ...this.data.entities,
           ...res.data.data.articles
         ];
         this.setData({
           entities,
-          isLoading:false,
+          isLoading: false,
           currentPage
         })
       }
     })
+    // this.data.currentPage
   }
 })
