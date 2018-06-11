1a17ec6d726d3bfcce3739cb7d45b511ba7c7389
zepengYu <2549793618@qq.com>
2018-06-11 23:17:12 +0800

    mpvue练习

---
+++ weapp/cnode/src/components/slider-nav.vue
@@ -0,0 +1,34 @@
+<template>
+    <scroll-view class="swiper-tab" scroll-x style="width: 100%">
+        <view v-for="(item,index) in navList" :key="index" class="swiper-tab-item">{{item.title}}</view>
+    </scroll-view>
+</template>
+<script>
+    export default{
+        name: "slider-nav",
+        props:{
+
+        navList:{
+            type:Array
+        }
+    }
+    }
+    
+</script>
+
+<style lang="scss" scoped>
+    .swiper-tab {
+        width: 100%;
+        text-align: center;
+        line-height: 42px;
+        white-space: nowrap;
+        position: relative;
+        .swiper-tab-item {
+            transition: all $time;
+            font-size: 18px;
+            height: 42px;
+            display: inline-block;
+            color: #777777;
+        }
+    }
+</style>
---
+++ weapp/cnode/src/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <view class="container">
+      <slider-nav :navList="this.navList"></slider-nav>
+    </view>
+</template>
+<script>
+  import sliderNav from '@/components/slider-nav'
+  import { navList } from '@/common/js/basic'
+  export default {
+    data(){
+      return{
+        navList:navList,
+      }
+
+    },
+    components:{
+      sliderNav
+    }
+    }
+
+    
+    
+</script>
+<style lang="scss" scoped>
+  .container{
+    height: 100%;
+  }
+
+</style>
+
