365c4e143f9f09441ee25bef6c417353040e031d
zepengYu <2549793618@qq.com>
2018-06-13 22:13:26 +0800

    完成了翻页

--- html5/fullpage/common.js
+++ html5/fullpage/common.js
@@ -3,10 +3,20 @@ class PureFullPage{
         // 1.手动计算page高度
         // #pureFullPage高度？
         // 属性定义
-        let a = document.documentElement.clientHeight;
-        console.log(a);
-        this.viewHeight = a;
+        // let a = document.documentElement.clientHeight;
+        // console.log(a);
+        const defaultOptions = {
+            isShowNav: true,
+            delay: 1000,
+            // 每次turnpage callback
+            definePages:() => {}
+        }
+        this.options = Object.assign(defaultOptions, options);
+        // console.log(this.options);
+        this.viewHeight = document.documentElement.clientHeight;
         this.container = document.getElementById('pureFullPage');
+        this.DELAY = this.options.delay;
+        this.currentPosition = 0;
         this.init();
     }
     init(){
@@ -20,7 +30,7 @@ class PureFullPage{
         // 从新返回一个函数，handleMouseWheel，闭包，将this.scrollMouse 封到内部
         // this，函数执行的context
         // 1000 delay 推迟执行
-        const handleMouseWheel = utils.throttle(this.scrollMouse, this, 1000)
+        const handleMouseWheel = utils.throttle(this.scrollMouse, this, this.DELAY);
         if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1){
             document.addEventListener('mousewheel', handleMouseWheel, false);
         } else {
@@ -28,10 +38,49 @@ class PureFullPage{
                 console.log('scroll');
             }, false);
         }
+
+        // 事件处理函数交由对象方法来执行
+        window.addEventListener('resize', this.handleWindowResize.bind(this), false);
     }
 
+    handleWindowResize(event){
+        // 对象的方法，肯定有需要找到对象
+        // this 代表对象
+        // console.log(this);
+        // 防抖
+        utils.debounce(this.getNewPosition, this, event, this.DELAY);
+    }
+
+    getNewPosition(){
+        console.log('debounce');
+        // 重新计算更改后的高度，并改变，动态的
+        this.viewHeight = document.documentElement.clientHeight;
+        this.container.style.height = `${this.viewHeight}px`;
+    }
     // 滚轮事件处理函数
-    scrollMouse(){
-        console.log(this.container);
+    scrollMouse(event){
+        // console.log(this.container);
+        const delta = utils.getWheelDelta(event);
+        // console.log(delta);
+        if(delta > 0){
+            // 向上滚
+            this.goUp();
+        } else {
+            // 向下滚
+            this.goDown();
+        }
+    }
+
+    goUp(){
+        console.log(this.currentPosition);
+        if(this.currentPosition == 0) return;
+        this.currentPosition += this.viewHeight;
+        this.container.style.top = `${this.currentPosition}px`
+    }
+    goDown(){
+        console.log(this.currentPosition);
+        if(this.currentPosition == -2*this.viewHeight) return;
+        this.currentPosition -= this.viewHeight;
+        this.container.style.top = `${this.currentPosition}px`;
     }
 }
--- html5/fullpage/utils.js
+++ html5/fullpage/utils.js
@@ -1,3 +1,10 @@
+
+// 截流 throttle，在规定时间只执行一次，先执行
+// debounce 防抖，在规定时间执行一次，后执行
+// 都为解决高频事件而来
+// scroll mousewheel mousemover moucemove touchmove onresize
+
+
 const utils = {
     // 帮忙method执行的次数在规定时间内只有一次
     // method执行时，函数内部的this purefullpage
@@ -21,5 +28,24 @@ const utils = {
             }
            
         }
+    },
+    debounce(method, context, event, delay){
+        // window.resize 不要急 等一下在执行
+        clearTimeout(context.tId);
+        // 定时器保持它的名字
+        // js 动态语言
+        context.tId = setTimeout(()=>{
+            method.call(context, event);
+        }, delay)
+        
+    },
+    getWheelDelta(event){
+        // console.log(event);
+        if(event.wheelDelta){
+            this.getWheelDelta = event => event.wheelDelta;
+            return event.wheelDelta;
+        }
+
     }
+
 }
