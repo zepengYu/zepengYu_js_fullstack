48f420d8a0d68edbe2a26eadc33e64a4846e4c15
zepengYu <2549793618@qq.com>
2018-07-24 21:23:19 +0800

    webpack学习(二)

---
+++ react/webpack_react/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve('dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.styl/,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'static/css/images/[name].[hash:8].[ext]'
+            }
+          }
+        ]        
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash:8].css',
+      chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      file: 'index.html',
+      template: 'public/index.html'
+    })
+  ],
+  devServer: {
+    port: 3002,
+    open: true
+  },
+  devtool: 'inline-source-map'
+}
