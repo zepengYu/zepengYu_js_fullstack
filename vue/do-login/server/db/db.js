cabe32898d4dde11e6937921256b631ab0173e53
zepengYu <2549793618@qq.com>
2018-08-07 22:03:59 +0800

    vue全栈练习(一)

---
+++ vue/do-login/server/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const config = require('config-lite')
+
+mongoose.connect(config.mongodb, {
+    useMongoClient: true
+})
+mongoose.Promise = global.promise
+
+var db = mongoose.connection
+db.once('open', function() {
+    console.log('MongoDB started successfully')
+})
