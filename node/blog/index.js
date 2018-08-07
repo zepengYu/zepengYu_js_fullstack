95757d168e35d40969653879f992679fe474de13
zepengYu <2549793618@qq.com>
2018-08-07 22:20:38 +0800

    后端搭建博客

---
+++ node/blog/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+// session 存储在内存，mongodb 放在数据库
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const config = require('config-lite')(__dirname)
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// 静态服务器
+// public/
+// 分布式的，不止一台服务器，
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(config.port, () => {
+    console.log(`listen on port ${config.port}`);
+    
+})
---
+++ node/myblog/readme.md
@@ -0,0 +1,20 @@
+koa express
+node server frame
+koa egg async awite 异步
+express 早一点
+
+单点入口
+
+app实例化，listen router 分发 服务器资源
+MVC
+router -> controller
+controller -> view
+view post|get -> controller -> model 
+
+/login 
+- contraller login.js
+    - login.ejs
+    - form
+        - contraller
+        login.js
+        - models/login.js save
